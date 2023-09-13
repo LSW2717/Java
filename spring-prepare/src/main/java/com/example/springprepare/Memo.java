9e23cda31180987b2a82210eec1c4649e829f4bc
Leesiwon12171260 <siwon2717@gmail.com>
2023-09-13 21:44:04 +0900

    Lombok

---
+++ spring-prepare/src/main/java/com/example/springprepare/Memo.java
@@ -0,0 +1,12 @@
+package com.example.springprepare;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Memo {
+    private String username;
+    private String contents;
+
+}
