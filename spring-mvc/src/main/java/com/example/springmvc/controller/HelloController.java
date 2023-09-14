78b1cde40d22b154dc452c466dc288ce6449229e
Leesiwon12171260 <siwon2717@gmail.com>
2023-09-14 20:55:48 +0900

    정적페이지와 동적페이지

--- spring-mvc/src/main/java/com/example/springmvc/controller/HelloController.java
+++ spring-mvc/src/main/java/com/example/springmvc/controller/HelloController.java
@@ -7,29 +7,35 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/api")
 public class HelloController {
+
+
     @GetMapping("/hello")
     @ResponseBody
-    public String hello(){
+    public String hello() {
         return "Hello World!!";
     }
+
     @GetMapping("/get")
     @ResponseBody
-    public String get(){
+    public String get() {
         return "GET Method 요청";
     }
+
     @PostMapping("/post")
     @ResponseBody
-    public String post(){
+    public String post() {
         return "POST Method 요청";
     }
+
     @PutMapping("/put")
     @ResponseBody
-    public String put(){
+    public String put() {
         return "PUT Method 요청";
     }
+
     @DeleteMapping("/delete")
     @ResponseBody
-    public String delete(){
+    public String delete() {
         return "DELETE Method 요청";
     }
 }
--- spring-prepare/src/main/java/com/example/springprepare/Memo.java
+++ spring-prepare/src/main/java/com/example/springprepare/Memo.java
@@ -1,10 +1,11 @@
 package com.example.springprepare;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Memo {
     private String username;
     private String contents;
