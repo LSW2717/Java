f3683566fa509be11b8080926887a1c690dde4f7
Leesiwon12171260 <siwon2717@gmail.com>
2023-09-14 21:23:06 +0900

    데이터를 client에 반환하는 방법

---
+++ spring-mvc/src/main/java/com/example/springmvc/response/ResponseRestController.java
@@ -0,0 +1,28 @@
+package com.example.springmvc.response;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController//RestController를 사용함으로써 templates에 안넣어도 됨
+@RequestMapping("/response/rest")
+public class ResponseRestController {
+    // [Response header]
+    //   Content-Type: text/html
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/string")
+    //Rest를 붙였기 때문에 @ResponseBody가 없어도됨
+    public String helloStringJson() {
+        return "{\"name\":\"Robbie\",\"age\":95}";
+    }
+
+    // [Response header]
+    //   Content-Type: application/json
+    // [Response body]
+    //   {"name":"Robbie","age":95}
+    @GetMapping("/json/class")
+    public Star helloClassJson() {
+        return new Star("Robbie", 95);
+    }
+}
